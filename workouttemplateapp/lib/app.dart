80dbe9077572ea9acf1200aca7dd1ca1eba7dd3e
Rollemann <suslikrolf@gmail.com>
2023-12-21 21:53:43 +0100

    tests

---
+++ workouttemplateapp/lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Workout APP',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Text("Test"),
+        ),
+        body: Container(
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
--- workouttemplateapp/lib/main.dart
+++ workouttemplateapp/lib/main.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:workouttemplateapp/app.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
+/* class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
 
@@ -66,3 +51,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+ */
