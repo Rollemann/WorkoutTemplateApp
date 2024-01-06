dd6539c2604651d881282bc52f2bbe878b7762e3
Rollemann <suslikrolf@gmail.com>
2024-01-06 23:56:41 +0100

    add and remove row

--- workouttemplateapp/lib/screens/templateDetails.dart
+++ workouttemplateapp/lib/screens/templateDetails.dart
@@ -1,28 +1,67 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:workouttemplateapp/screens/templateRow.dart';
 
-class TemplateDetails extends StatelessWidget {
-  TemplateDetails({super.key});
+class TemplateDetails extends StatefulWidget {
+  const TemplateDetails({super.key});
 
-  final List<TemplateRow> templateRows = [
-    const TemplateRow(),
-    const TemplateRow(),
-    const TemplateRow(),
+  @override
+  State<TemplateDetails> createState() => _TemplateDetailsState();
+}
+
+class _TemplateDetailsState extends State<TemplateDetails> {
+  final listKey = GlobalKey<AnimatedListState>();
+  List<String> templateRows = [
+    "test1",
+    "test2",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
+        ElevatedButton(
+          onPressed: () => {addRow(0)},
+          style: const ButtonStyle(
+              shape: MaterialStatePropertyAll(CircleBorder())),
+          child: const Icon(Icons.add),
+        ),
         Expanded(
-          child: ListView.builder(
-            itemCount: templateRows.length,
-            itemBuilder: (BuildContext context, int index) {
-              return templateRows[index];
-            },
+          child: AnimatedList(
+            key: listKey,
+            initialItemCount: templateRows.length,
+            itemBuilder: (context, index, animation) => TemplateRow(
+                text: templateRows[index],
+                animation: animation,
+                addRow: () => addRow(index),
+                removeRow: () => removeRow(index)),
           ),
         ),
       ],
     );
+  }
+
+  void addRow(int index) {
+    final newIndex = index+1; 
+    final newRow = "Test $newIndex";
+    templateRows.insert(newIndex, newRow);
+    listKey.currentState!.insertItem(newIndex);
+  }
+
+  void removeRow(int index) {
+    final removedRow = templateRows[index];
+
+    templateRows.removeAt(index);
+
+    listKey.currentState!.removeItem(
+        index,
+        (context, animation) => TemplateRow(
+              text: removedRow,
+              animation: animation,
+              addRow: () {},
+              removeRow: () {},
+            ));
   }
 }
--- workouttemplateapp/lib/screens/templateRow.dart
+++ workouttemplateapp/lib/screens/templateRow.dart
@@ -1,105 +1,119 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
 class TemplateRow extends StatelessWidget {
-  const TemplateRow({super.key});
+  final String text;
+  final Animation<double> animation;
+  final VoidCallback addRow;
+  final VoidCallback removeRow;
+
+  const TemplateRow({
+    super.key,
+    required this.text,
+    required this.animation,
+    required this.addRow,
+    required this.removeRow,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            ElevatedButton(
-              onPressed: () => {log("hi")},
-              style: const ButtonStyle(
-                  shape: MaterialStatePropertyAll(CircleBorder())),
-              child: const Icon(Icons.add),
-            ),
-          ],
-        ),
-        Container(
-          color: Colors.grey[300],
-          child: Row(
-            children: [
-              const Flexible(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Flexible(
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: SizedBox(
-                              width: 30.0,
+    return SizeTransition(
+      //key: ValueKey(text), TODO: falls flackern auftritt beim löschen
+      sizeFactor: animation,
+      child: Column(
+        children: [
+          Container(
+            color: Colors.grey[300],
+            child: Row(
+              children: [
+                Flexible(
+                  child: Column(
+                    children: [
+                      const Row(
+                        children: [
+                          Flexible(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: SizedBox(
+                                width: 30.0,
+                                child: TextField(
+                                  textAlign: TextAlign.center,
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    labelText: 'Set',
+                                    border: UnderlineInputBorder(),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Flexible(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
                               child: TextField(
                                 textAlign: TextAlign.center,
-                                keyboardType: TextInputType.number,
                                 decoration: InputDecoration(
-                                  labelText: 'Set',
+                                  labelText: 'Weight',
                                   border: UnderlineInputBorder(),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                        Flexible(
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: TextField(
-                              textAlign: TextAlign.center,
-                              decoration: InputDecoration(
-                                labelText: 'Weight',
-                                border: UnderlineInputBorder(),
+                          Flexible(
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  labelText: 'Reps',
+                                  border: UnderlineInputBorder(),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        Flexible(
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: TextField(
-                              textAlign: TextAlign.center,
-                              keyboardType: TextInputType.number,
-                              decoration: InputDecoration(
-                                labelText: 'Reps',
-                                border: UnderlineInputBorder(),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Flexible(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextField(
+                                controller: TextEditingController(text: text),
+                                decoration: const InputDecoration(
+                                  labelText: 'Exercise',
+                                  border: UnderlineInputBorder(),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Flexible(
-                          child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: TextField(
-                              decoration: InputDecoration(
-                                labelText: 'Exercise',
-                                border: UnderlineInputBorder(),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+                ElevatedButton(
+                  onPressed: () => {removeRow()},
+                  style: const ButtonStyle(
+                      shape: MaterialStatePropertyAll(CircleBorder())),
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
               ElevatedButton(
-                onPressed: () => {log("bye")},
+                onPressed: () => {addRow()},
                 style: const ButtonStyle(
                     shape: MaterialStatePropertyAll(CircleBorder())),
-                child: const Icon(Icons.remove),
+                child: const Icon(Icons.add),
               ),
             ],
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
